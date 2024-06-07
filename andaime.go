0e5ea2b3fe5a0629fb0c4d2d19593623358bf424
seanmtracey <seanmtracey@gmail.com>
2024-06-07 06:46:10 +0100

    Boilerplate AWS Scaffold;

--- andaime.go
+++ andaime.go
@@ -6,6 +6,10 @@ import(
 	"strconv"
 	"flag"
 	"encoding/json"
+
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/ec2"
 )
 
 var (
@@ -28,6 +32,119 @@ var (
 	NUMBER_OF_ORCHESTRATOR_NODES_FLAG  int
 	NUMBER_OF_COMPUTE_NODES_FLAG       int
 )
+
+func getAmazonLinuxAMIId(svc *ec2.EC2) (string, error) {
+
+	describeImagesInput := &ec2.DescribeImagesInput{
+        Filters: []*ec2.Filter{
+            {
+                Name:   aws.String("name"),
+                Values: aws.StringSlice([]string{"amzn2-ami-hvm-*-arm64-gp2"}),
+            },
+            {
+                Name:   aws.String("architecture"),
+                Values: aws.StringSlice([]string{"arm64"}),
+            },
+            {
+                Name:   aws.String("state"),
+                Values: aws.StringSlice([]string{"available"}),
+            },
+        },
+        Owners: aws.StringSlice([]string{"amazon"}),
+    }
+
+    // Call DescribeImages to find matching AMIs
+    result, err := svc.DescribeImages(describeImagesInput)
+    if err != nil {
+        fmt.Println("failed to describe images, %v", err)
+		return "", err
+    }
+
+    if len(result.Images) == 0 {
+        fmt.Println("no Amazon Linux 2023 AMIs found")
+		return "", err
+    }
+
+    // Get the latest image (you might want to add more logic to select the right one)
+    latestImage := result.Images[0]
+    for _, image := range result.Images {
+        if *image.CreationDate > *latestImage.CreationDate {
+            latestImage = image
+        }
+    }
+
+    fmt.Printf("Using AMI ID: %s\n", *latestImage.ImageId)
+	return *latestImage.ImageId, nil
+
+}
+
+func DeployOnAWS(){
+	// Create a new session using the default configuration
+    sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("us-east-1"), // specify the region you want
+    })
+    if err != nil {
+        fmt.Println("failed to create session, %v", err)
+		os.Exit(1)
+
+    }
+
+    svc := ec2.New(sess)
+
+    blockDeviceMappings := []*ec2.BlockDeviceMapping{
+        {
+            DeviceName: aws.String("/dev/xvda"),
+            Ebs: &ec2.EbsBlockDevice{
+                VolumeSize:          aws.Int64(20),
+                VolumeType:          aws.String("gp2"),
+                DeleteOnTermination: aws.Bool(true),
+            },
+        },
+    }
+
+	amazonLinuxAMI, amiErr := getAmazonLinuxAMIId(svc)
+
+	if amiErr != nil {
+		fmt.Println("Could not find AMI ID for Amazon Linux")
+		os.Exit(1)
+	}
+
+	fmt.Println(amazonLinuxAMI)
+	os.Exit(0)
+
+	// TODO:
+	// Get default VPC for target region
+	// Get Subnets for found VPC
+
+    // Define the instance parameters
+    runInstancesInput := &ec2.RunInstancesInput{
+        ImageId:             aws.String(amazonLinuxAMI),
+        InstanceType:        aws.String("t4g.nano"),
+        MinCount:            aws.Int64(1),
+        MaxCount:            aws.Int64(1),
+        BlockDeviceMappings: blockDeviceMappings,
+        NetworkInterfaces: []*ec2.InstanceNetworkInterfaceSpecification{
+            {
+                AssociatePublicIpAddress: aws.Bool(true),
+                DeviceIndex:              aws.Int64(0),
+                SubnetId:                 aws.String(""),
+            },
+        },
+        KeyName: aws.String("my-key-pair"), // replace with your key pair
+    }
+
+    // Run the instance
+    result, err := svc.RunInstances(runInstancesInput)
+    if err != nil {
+        fmt.Println("failed to run instance, %v", err)
+		os.Exit(1)
+    }
+
+    // Print the instance ID of the newly created instance
+    for _, instance := range result.Instances {
+        fmt.Printf("Successfully launched instance %s\n", *instance.InstanceId)
+    }
+}
 
 func ProcessEnvVars(){
 
@@ -217,14 +334,6 @@ func main(){
 	// Parse flags first, but set later so we can check whether
 	// or not the user has passed the --verbose flag
 
-	flag.BoolVar(&VERBOSE_MODE_FLAG, "verbose", false, "Generate verbose output throughout execution")
-	flag.StringVar(&PROJECT_NAME_FLAG, "project-name", "", "Set project name")
-    flag.StringVar(&TARGET_PLATFORM_FLAG, "target-platform", "", "Set target platform")
-    flag.IntVar(&NUMBER_OF_ORCHESTRATOR_NODES_FLAG, "orchestrator-nodes", -1, "Set number of orchestrator nodes")
-    flag.IntVar(&NUMBER_OF_COMPUTE_NODES_FLAG, "compute-nodes", -1, "Set number of compute nodes")
-
-	flag.Parse()
-
 	ProcessEnvVars()
 	configErr := ProcessConfigFile()
 
@@ -232,6 +341,14 @@ func main(){
 		fmt.Println("Error reading configuration file:", configErr)
 	}
 
+	flag.BoolVar(&VERBOSE_MODE_FLAG, "verbose", false, "Generate verbose output throughout execution")
+	flag.StringVar(&PROJECT_NAME_FLAG, "project-name", "", "Set project name")
+    flag.StringVar(&TARGET_PLATFORM_FLAG, "target-platform", "", "Set target platform")
+    flag.IntVar(&NUMBER_OF_ORCHESTRATOR_NODES_FLAG, "orchestrator-nodes", -1, "Set number of orchestrator nodes")
+    flag.IntVar(&NUMBER_OF_COMPUTE_NODES_FLAG, "compute-nodes", -1, "Set number of compute nodes")
+
+	flag.Parse()
+	
 	ProcessFlags()
 
 	fmt.Println("Project configuration:\n")
@@ -240,5 +357,7 @@ func main(){
 	fmt.Printf("\tNo. of Orchestrator Nodes: %d (set by %s)\n", PROJECT_SETTINGS["NumberOfOrchestratorNodes"], SET_BY["NumberOfOrchestratorNodes"])
 	fmt.Printf("\tNo. of Compute Nodes: %d (set by %s)\n", PROJECT_SETTINGS["NumberOfComputeNodes"], SET_BY["NumberOfComputeNodes"] )
 	fmt.Print("\n")
+
+	DeployOnAWS()
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module andaime
 
 go 1.22.2
+
+require (
+	github.com/aws/aws-sdk-go v1.53.10 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
