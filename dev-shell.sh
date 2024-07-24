fe1c2ccd2594a86455f1d5fea464d4292a683a14
David Aronchick <aronchick@gmail.com>
2024-07-24 10:39:06 -0700

    Added a development shell script and updated the test runner script to print the Go version before running the tests.

---
+++ dev-shell.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Enter the Nix shell for development
+nix-shell
--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Enter the Nix shell and run the tests
-nix-shell --run "go test ./..."
+nix-shell --run "go version && go test ./..."
