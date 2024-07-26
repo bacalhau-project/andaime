427c69737eaebe31702bd4574104704b52a4122c
David Aronchick <aronchick@gmail.com>
2024-07-25 17:20:47 -0700

    Defined an AzureProviderer interface with a DestroyAzureDeployment method.

--- pkg/providers/azure/destroy.go
+++ pkg/providers/azure/destroy.go
@@ -7,6 +7,10 @@ import (
 	"github.com/bacalhau-project/andaime/pkg/logger"
 )
 
+type AzureProviderer interface {
+	DestroyAzureDeployment(ctx context.Context, resourceGroupName string) error
+}
+
 // DestroyAzureDeployment deletes the specified Azure resource group
 func (p *AzureProvider) DestroyAzureDeployment(ctx context.Context, resourceGroupName string) error {
 	l := logger.Get()
