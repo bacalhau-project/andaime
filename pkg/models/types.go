a88819adfed204568fd2ef5601d78e8324cca225
David Aronchick <aronchick@gmail.com>
2024-07-27 16:59:24 -0700

    Added a new Deployment struct to the models package.

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -20,6 +20,23 @@ type Status struct {
 	StartTime       time.Time
 	InstanceID      string
 	PublicIP        string
+}
+
+type Deployment struct {
+	ProjectID             string
+	UniqueID              string
+	ResourceGroupName     string
+	ResourceGroupLocation string
+	Machines              []*Machine
+	OrchestratorNode      *Machine
+	NetworkSecurityGroups map[string]*armnetwork.SecurityGroup
+	Subnets               map[string][]*armnetwork.Subnet
+	SSHPublicKeyPath      string
+	SSHPrivateKeyPath     string
+	SSHPublicKeyData      []byte
+	AllowedPorts          []int
+	Tags                  map[string]*string
+	StartTime             time.Time
 	PrivateIP       string
 	HighlightCycles int
 }
