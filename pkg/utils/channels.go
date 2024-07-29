999cf163f932cc9884a3643b4ac439289b52cadb
David Aronchick <aronchick@gmail.com>
2024-07-29 15:36:37 -0700

    Refactored the channel management utilities, introducing a `SafeChannel` struct and helper functions for creating and managing channels.

--- pkg/utils/channels.go
+++ pkg/utils/channels.go
@@ -1,72 +1,61 @@
 package utils
 
 import (
+	"os"
+	"reflect"
 	"sync"
 
 	"github.com/bacalhau-project/andaime/pkg/logger"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto/events"
 )
 
-var GlobalChannels []chan struct{}
+var GlobalChannels []interface {
+	Close()
+	IsClosed() bool
+}
 var GlobalChannelsMutex sync.Mutex
 
-type SafeChannel[T any] struct {
-	Ch     chan T
+type SafeChannel struct {
+	Ch     interface{}
 	Closed bool
 	Mu     sync.Mutex
 }
 
-func NewSafeChannel[T any](capacity int) SafeChannel[T] {
-	return SafeChannel[T]{
-		Ch:     make(chan T, capacity),
-		Closed: true,
+func NewSafeChannel(ch interface{}) *SafeChannel {
+	return &SafeChannel{
+		Ch:     ch,
+		Closed: false,
 		Mu:     sync.Mutex{},
 	}
 }
-func (sc *SafeChannel[T]) Send(value T) bool {
+
+func (sc *SafeChannel) Close() {
 	sc.Mu.Lock()
 	defer sc.Mu.Unlock()
 	if !sc.Closed {
-		sc.Ch <- value
-		return true
-	}
-	return false
-}
-
-func (sc *SafeChannel[T]) Receive() (T, bool) {
-	sc.Mu.Lock()
-	defer sc.Mu.Unlock()
-	if sc.Closed {
-		var zero T
-		return zero, false
-	}
-	select {
-	case value := <-sc.Ch:
-		return value, true
-	default:
-		var zero T
-		return zero, false
-	}
-}
-
-func (sc *SafeChannel[T]) Close() {
-	sc.Mu.Lock()
-	defer sc.Mu.Unlock()
-	if !sc.Closed {
-		close(sc.Ch)
+		reflect.ValueOf(sc.Ch).Close()
 		sc.Closed = true
 	}
 }
 
-func (sc *SafeChannel[T]) IsClosed() bool {
+func (sc *SafeChannel) IsClosed() bool {
 	sc.Mu.Lock()
 	defer sc.Mu.Unlock()
 	return sc.Closed
 }
 
-func RegisterChannel(ch chan struct{}) {
+// CreateAndRegisterChannel creates a new channel of the specified type and capacity, and registers it
+func CreateAndRegisterChannel(channelType reflect.Type, capacity int) interface{} {
+	ch := reflect.MakeChan(channelType, capacity).Interface()
+	RegisterChannel(ch)
+	return ch
+}
+
+func RegisterChannel(ch interface{}) {
+	safeChannel := NewSafeChannel(ch)
 	GlobalChannelsMutex.Lock()
 	defer GlobalChannelsMutex.Unlock()
-	GlobalChannels = append(GlobalChannels, ch)
+	GlobalChannels = append(GlobalChannels, safeChannel)
 }
 
 func CloseAllChannels() {
@@ -77,8 +66,58 @@ func CloseAllChannels() {
 	for _, ch := range GlobalChannels {
 		l.Debugf("Closing channel %v", ch)
 		if ch != nil {
-			close(ch)
+			ch.Close()
 		}
 	}
 	GlobalChannels = nil
+}
+
+// Helper functions for common channel types
+func CreateSignalChannel(capacity int) chan os.Signal {
+	return CreateAndRegisterChannel(reflect.TypeOf((*chan os.Signal)(nil)).Elem(), capacity).(chan os.Signal)
+}
+
+func CreateStructChannel(capacity int) chan struct{} {
+	return CreateAndRegisterChannel(reflect.TypeOf((*chan struct{})(nil)).Elem(), capacity).(chan struct{})
+}
+
+func CreateErrorChannel(capacity int) chan error {
+	return CreateAndRegisterChannel(reflect.TypeOf((*chan error)(nil)).Elem(), capacity).(chan error)
+}
+
+func CreateStringChannel(capacity int) chan string {
+	return CreateAndRegisterChannel(reflect.TypeOf((*chan string)(nil)).Elem(), capacity).(chan string)
+}
+
+func CreateEventChannel(capacity int) chan events.EngineEvent {
+	return CreateAndRegisterChannel(reflect.TypeOf((*chan events.EngineEvent)(nil)).Elem(),
+		capacity).(chan events.EngineEvent)
+}
+
+// CloseChannel closes a specific channel
+func CloseChannel(ch interface{}) {
+	GlobalChannelsMutex.Lock()
+	defer GlobalChannelsMutex.Unlock()
+	for i, safeChannel := range GlobalChannels {
+		if reflect.ValueOf(safeChannel).Pointer() == reflect.ValueOf(ch).Pointer() {
+			safeChannel.Close()
+			// Remove the closed channel from the GlobalChannels slice
+			GlobalChannels = append(GlobalChannels[:i], GlobalChannels[i+1:]...)
+			return
+		}
+	}
+	// If the channel wasn't found, it might not have been registered
+	l := logger.Get()
+	l.Warnf("Attempted to close an unregistered channel: %v", ch)
+}
+
+func IsChannelClosed(ch interface{}) bool {
+	GlobalChannelsMutex.Lock()
+	defer GlobalChannelsMutex.Unlock()
+	for _, safeChannel := range GlobalChannels {
+		if reflect.ValueOf(safeChannel).Pointer() == reflect.ValueOf(ch).Pointer() {
+			return safeChannel.IsClosed()
+		}
+	}
+	return true
 }
