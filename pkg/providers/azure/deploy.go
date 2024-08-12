12b32b4ce0613a4c29bcf8faf894d2dcbac14e72
David Aronchick <aronchick@gmail.com>
2024-08-12 10:45:50 -0700

    fix: Update deployment machine status and elapsed time

--- pkg/providers/azure/deploy.go
+++ pkg/providers/azure/deploy.go
@@ -355,8 +355,8 @@ func (p *AzureProvider) deployTemplateWithRetry(
 
 			deployment.Machines[machineIndex].PublicIP = publicIP
 			deployment.Machines[machineIndex].PrivateIP = privateIP
+			deployment.Machines[machineIndex].ElapsedTime = time.Since(machine.StartTime)
 			deployment.Machines[machineIndex].Status = "Successfully Deployed"
-			elapsedTime := time.Since(machine.StartTime)
 			disp.UpdateStatus(
 				&models.Status{
 					ID:          machine.Name,
@@ -364,7 +364,7 @@ func (p *AzureProvider) deployTemplateWithRetry(
 					Status:      "Successfully Deployed.",
 					PublicIP:    deployment.Machines[machineIndex].PublicIP,
 					PrivateIP:   deployment.Machines[machineIndex].PrivateIP,
-					ElapsedTime: elapsedTime,
+					ElapsedTime: deployment.Machines[machineIndex].ElapsedTime,
 				},
 			)
 			break
