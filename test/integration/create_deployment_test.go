3854721b8d981741e62cad265df8a53a57cd1510
David Aronchick <aronchick@gmail.com>
2024-09-09 19:16:30 -0700

    feat: Add test mode environment variable and mock SSH config

--- test/integration/create_deployment_test.go
+++ test/integration/create_deployment_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
@@ -20,10 +21,10 @@ import (
 	"github.com/bacalhau-project/andaime/cmd/beta/azure"
 	"github.com/bacalhau-project/andaime/cmd/beta/gcp"
 	"github.com/bacalhau-project/andaime/internal/testutil"
-	"github.com/bacalhau-project/andaime/pkg/common"
 	"github.com/bacalhau-project/andaime/pkg/display"
 	"github.com/bacalhau-project/andaime/pkg/models"
 	azure_provider "github.com/bacalhau-project/andaime/pkg/providers/azure"
+	"github.com/bacalhau-project/andaime/pkg/providers/common"
 	gcp_provider "github.com/bacalhau-project/andaime/pkg/providers/gcp"
 	"github.com/bacalhau-project/andaime/pkg/sshutils"
 	tea "github.com/charmbracelet/bubbletea"
@@ -119,6 +120,10 @@ func (m *MockSSHConfig) WaitForSSH(ctx context.Context, ip string, port int, use
 }
 
 func TestExecuteCreateDeployment(t *testing.T) {
+	// Set the test mode environment variable
+	os.Setenv("ANDAIME_TEST_MODE", "true")
+	defer os.Unsetenv("ANDAIME_TEST_MODE")
+
 	testSSHPublicKeyPath,
 		cleanupPublicKey,
 		testSSHPrivateKeyPath,
@@ -135,14 +140,16 @@ func TestExecuteCreateDeployment(t *testing.T) {
 	mockSSHConfig.On("WaitForSSH", mock.Anything,
 		mock.Anything,
 		mock.Anything,
-		mock.Anything).Return(nil)
+		mock.Anything,
+		mock.Anything,
+	).Return(nil)
 	mockSSHConfig.On("PushFile", mock.Anything,
+		mock.Anything,
 		mock.Anything,
 		mock.Anything,
 		mock.Anything,
 	).Return(nil)
 	mockSSHConfig.On("ExecuteCommand", mock.Anything,
-		mock.Anything,
 		mock.Anything,
 		mock.Anything,
 		mock.Anything,
@@ -176,7 +183,8 @@ func TestExecuteCreateDeployment(t *testing.T) {
 		{"GCP", "gcp"},
 	}
 
-	for _, tt := range tests {
+	for i := range tests {
+		tt := tests[i]
 		t.Run(tt.name, func(t *testing.T) {
 			viper.Reset()
 			viper.Set("general.project_prefix", "test-1292")
@@ -208,21 +216,6 @@ func TestExecuteCreateDeployment(t *testing.T) {
 			display.GetGlobalProgramFunc = func() display.GlobalProgammer {
 				return mockProgram
 			}
-
-			mockSSHConfig := new(sshutils.MockSSHConfig)
-			mockSSHConfig.On(
-				"WaitForSSH",
-				mock.Anything,
-				mock.Anything,
-				mock.Anything,
-			).Return(nil)
-			mockSSHConfig.On(
-				"ExecuteCommand",
-				mock.Anything,
-				mock.Anything,
-				mock.Anything,
-				mock.Anything,
-			).Return(nil)
 
 			if tt.provider == "azure" {
 				// Generic Resource Group
@@ -308,7 +301,9 @@ func TestExecuteCreateDeployment(t *testing.T) {
 				viper.Set("azure.default_count_per_zone", 1)
 				viper.Set("azure.default_location", "eastus2")
 				viper.Set("azure.default_machine_type", "Standard_DS4_v2")
-				viper.Set("azure.disk_size_gb", 30)
+				viper.Set("azure.resource_group_location", "eastus2")
+				viper.Set("azure.resource_group_name", "test-1292-rg")
+				viper.Set("azure.default_disk_size_gb", 30)
 				viper.Set("azure.machines", []map[string]interface{}{
 					{
 						"location": "eastus2",
@@ -329,16 +324,60 @@ func TestExecuteCreateDeployment(t *testing.T) {
 				err = azure.ExecuteCreateDeployment(cmd, []string{})
 				mockAzureClient.AssertExpectations(t)
 			} else {
+				instances := []*computepb.Instance{
+					{
+						Name: to.Ptr("test-1292-instance-1"),
+						NetworkInterfaces: []*computepb.NetworkInterface{
+							{
+								AccessConfigs: []*computepb.AccessConfig{
+									{
+										NatIP: to.Ptr("256.256.256.256"),
+									},
+								},
+								NetworkIP: to.Ptr("10.0.0.4"),
+							},
+						},
+					},
+					{
+						Name: to.Ptr("test-1292-instance-2"),
+						NetworkInterfaces: []*computepb.NetworkInterface{
+							{
+								AccessConfigs: []*computepb.AccessConfig{
+									{
+										NatIP: to.Ptr("256.256.256.257"),
+									},
+								},
+								NetworkIP: to.Ptr("10.0.0.5"),
+							},
+						},
+					},
+				}
+
 				viper.Set("gcp.project_id", "test-1292-gcp")
 				viper.Set("gcp.organization_id", "org-1234567890")
 				viper.Set("gcp.billing_account_id", "123456-789012-345678")
+				viper.Set("gcp.default_count_per_zone", 1)
+				viper.Set("gcp.default_location", "us-central1-a")
+				viper.Set("gcp.default_machine_type", "n2-highcpu-4")
+				viper.Set("gcp.default_disk_size_gb", 30)
+				viper.Set("gcp.machines", []map[string]interface{}{
+					{
+						"location": "us-central1-a",
+						"parameters": map[string]interface{}{
+							"count": 2,
+						},
+					},
+					{
+						"location": "us-central1-b",
+						"parameters": map[string]interface{}{
+							"orchestrator": true,
+						},
+					},
+				})
 
 				mockGCPClient := new(gcp_provider.MockGCPClient)
-				mockGCPClient.On("ValidateMachineType", mock.Anything, mock.Anything, mock.Anything).
-					Return(true, nil)
 				mockGCPClient.On("EnsureProject", mock.Anything, mock.Anything, mock.Anything).
 					Return("test-1292-gcp", nil)
-				mockGCPClient.On("StartResourcePolling", mock.Anything).Return(nil)
 				mockGCPClient.On("IsAPIEnabled", mock.Anything,
 					mock.Anything,
 					mock.Anything,
@@ -348,6 +387,11 @@ func TestExecuteCreateDeployment(t *testing.T) {
 					mock.Anything,
 					mock.Anything,
 				).Return(nil)
+				mockGCPClient.On("CreateComputeInstance",
+					mock.Anything,
+					mock.Anything,
+					mock.Anything,
+				).Return(instances[0], nil)
 
 				origNewGCPClientFunc := gcp_provider.NewGCPClientFunc
 				t.Cleanup(func() { gcp_provider.NewGCPClientFunc = origNewGCPClientFunc })
@@ -371,38 +415,6 @@ func TestPrepareDeployment(t *testing.T) {
 	defer cleanupPrivateKey()
 
 	mockSSHConfig := new(sshutils.MockSSHConfig)
-	// mockSSHConfig.On("ExecuteCommand", mock.Anything, mock.MatchedBy(func(command string) bool {
-	// 	return strings.Contains(command, "bacalhau node list --output json --api-host")
-	// })).
-	// 	Return(`[{"id": "node1"}]`, nil)
-
-	// mockSSHConfig.On("WaitForSSH", mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything).Return(nil)
-	// mockSSHConfig.On("PushFile", mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// ).Return(nil)
-	// mockSSHConfig.On("ExecuteCommand", mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// ).Return("", nil)
-	// mockSSHConfig.On("InstallSystemdService", mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// ).Return(nil)
-	// mockSSHConfig.On("RestartService", mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// 	mock.Anything,
-	// ).Return(nil)
 	sshutils.NewSSHConfigFunc = func(host string,
 		port int,
 		user string,
@@ -411,13 +423,15 @@ func TestPrepareDeployment(t *testing.T) {
 	}
 	tests := []struct {
 		name     string
-		provider string
+		errorMsg string
+		provider models.DeploymentType
 	}{
-		{"Azure", "azure"},
-		{"GCP", "gcp"},
+		{name: "Azure", errorMsg: "", provider: models.DeploymentTypeAzure},
+		{name: "GCP", errorMsg: "", provider: models.DeploymentTypeGCP},
 	}
 
-	for _, tt := range tests {
+	for i := range tests {
+		tt := tests[i]
 		t.Run(tt.name, func(t *testing.T) {
 			ctx := context.Background()
 			viper.Reset()
@@ -443,15 +457,19 @@ func TestPrepareDeployment(t *testing.T) {
 			assert.NoError(t, viper.ReadInConfig())
 
 			// Prevent tea program from rendering
-			os.Setenv("TEST_MODE", "true")
-			defer os.Unsetenv("TEST_MODE")
+			os.Setenv("ANDAIME_TEST_MODE", "true")
+			defer os.Unsetenv("ANDAIME_TEST_MODE")
 
-			if tt.provider == "azure" {
+			var deployment *models.Deployment
+			deployment, err = common.PrepareDeployment(ctx, tt.provider)
+			m := display.NewDisplayModel(deployment)
+
+			if tt.provider == models.DeploymentTypeAzure {
 				viper.Set("azure.subscription_id", "4a45a76b-5754-461d-84a1-f5e47b0a7198")
 				viper.Set("azure.default_count_per_zone", 1)
 				viper.Set("azure.default_location", "eastus2")
 				viper.Set("azure.default_machine_type", "Standard_D2_v2")
-				viper.Set("azure.disk_size_gb", 30)
+				viper.Set("azure.default_disk_size_gb", 30)
 				viper.Set("azure.machines", []map[string]interface{}{
 					{
 						"location": "australiasoutheast",
@@ -475,14 +493,14 @@ func TestPrepareDeployment(t *testing.T) {
 				azure_provider.NewAzureClientFunc = func(subscriptionID string) (azure_provider.AzureClienter, error) {
 					return mockAzureClient, nil
 				}
-
-			} else {
+			} else if tt.provider == models.DeploymentTypeGCP {
 				viper.Set("gcp.project_id", "test-1292-gcp")
 				viper.Set("gcp.organization_id", "org-1234567890")
 				viper.Set("gcp.billing_account_id", "123456-789012-345678")
 				viper.Set("gcp.default_count_per_zone", 1)
 				viper.Set("gcp.default_location", "me-central1-a")
 				viper.Set("gcp.default_machine_type", "n2-highcpu-4")
+				viper.Set("gcp.default_disk_size_gb", 30)
 				viper.Set("gcp.machines", []map[string]interface{}{
 					{
 						"location": "me-central1-a",
@@ -499,14 +517,17 @@ func TestPrepareDeployment(t *testing.T) {
 				})
 
 			}
-
-			var deployment *models.Deployment
-			deployment, err = common.PrepareDeployment(ctx, tt.provider)
+			err = common.ProcessMachinesConfig(
+				tt.provider,
+				func(ctx context.Context, location string, machineType string) (bool, error) {
+					return true, nil
+				},
+			)
 
 			assert.NoError(t, err)
-			assert.NotNil(t, deployment)
-			assert.NotEmpty(t, deployment.Name)
-			assert.NotEmpty(t, deployment.Machines)
+			assert.NotNil(t, m.Deployment)
+			assert.NotEmpty(t, m.Deployment.Name)
+			assert.NotEmpty(t, m.Deployment.Machines)
 		})
 	}
 }
