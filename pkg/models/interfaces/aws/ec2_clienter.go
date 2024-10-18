f3f2be88408064803e88d166ade47b6a359d12ed
David Aronchick <aronchick@gmail.com>
2024-10-18 08:47:52 -0700

    refactor: Refactor EC2Clienter interface to improve readability

--- pkg/models/interfaces/aws/ec2_clienter.go
+++ pkg/models/interfaces/aws/ec2_clienter.go
@@ -8,16 +8,64 @@ import (
 
 //go:generate mockery --name EC2Clienter
 type EC2Clienter interface {
-	RunInstances(ctx context.Context, params *ec2.RunInstancesInput, optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
-	DescribeInstances(ctx context.Context, params *ec2.DescribeInstancesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeInstancesOutput, error)
-	TerminateInstances(ctx context.Context, params *ec2.TerminateInstancesInput, optFns ...func(*ec2.Options)) (*ec2.TerminateInstancesOutput, error)
-	DescribeImages(ctx context.Context, params *ec2.DescribeImagesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeImagesOutput, error)
-	CreateSubnet(ctx context.Context, params *ec2.CreateSubnetInput, optFns ...func(*ec2.Options)) (*ec2.CreateSubnetOutput, error)
-	CreateVpc(ctx context.Context, params *ec2.CreateVpcInput, optFns ...func(*ec2.Options)) (*ec2.CreateVpcOutput, error)
-	DescribeVpcs(ctx context.Context, params *ec2.DescribeVpcsInput, optFns ...func(*ec2.Options)) (*ec2.DescribeVpcsOutput, error)
-	CreateInternetGateway(ctx context.Context, params *ec2.CreateInternetGatewayInput, optFns ...func(*ec2.Options)) (*ec2.CreateInternetGatewayOutput, error)
-	AttachInternetGateway(ctx context.Context, params *ec2.AttachInternetGatewayInput, optFns ...func(*ec2.Options)) (*ec2.AttachInternetGatewayOutput, error)
-	CreateRouteTable(ctx context.Context, params *ec2.CreateRouteTableInput, optFns ...func(*ec2.Options)) (*ec2.CreateRouteTableOutput, error)
-	CreateRoute(ctx context.Context, params *ec2.CreateRouteInput, optFns ...func(*ec2.Options)) (*ec2.CreateRouteOutput, error)
-	AssociateRouteTable(ctx context.Context, params *ec2.AssociateRouteTableInput, optFns ...func(*ec2.Options)) (*ec2.AssociateRouteTableOutput, error)
+	RunInstances(
+		ctx context.Context,
+		params *ec2.RunInstancesInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.RunInstancesOutput, error)
+	DescribeInstances(
+		ctx context.Context,
+		params *ec2.DescribeInstancesInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.DescribeInstancesOutput, error)
+	TerminateInstances(
+		ctx context.Context,
+		params *ec2.TerminateInstancesInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.TerminateInstancesOutput, error)
+	DescribeImages(
+		ctx context.Context,
+		params *ec2.DescribeImagesInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.DescribeImagesOutput, error)
+	CreateSubnet(
+		ctx context.Context,
+		params *ec2.CreateSubnetInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.CreateSubnetOutput, error)
+	CreateVpc(
+		ctx context.Context,
+		params *ec2.CreateVpcInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.CreateVpcOutput, error)
+	DescribeVpcs(
+		ctx context.Context,
+		params *ec2.DescribeVpcsInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.DescribeVpcsOutput, error)
+	CreateInternetGateway(
+		ctx context.Context,
+		params *ec2.CreateInternetGatewayInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.CreateInternetGatewayOutput, error)
+	AttachInternetGateway(
+		ctx context.Context,
+		params *ec2.AttachInternetGatewayInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.AttachInternetGatewayOutput, error)
+	CreateRouteTable(
+		ctx context.Context,
+		params *ec2.CreateRouteTableInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.CreateRouteTableOutput, error)
+	CreateRoute(
+		ctx context.Context,
+		params *ec2.CreateRouteInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.CreateRouteOutput, error)
+	AssociateRouteTable(
+		ctx context.Context,
+		params *ec2.AssociateRouteTableInput,
+		optFns ...func(*ec2.Options),
+	) (*ec2.AssociateRouteTableOutput, error)
 }
