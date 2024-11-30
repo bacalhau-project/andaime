88d119f305bf956775c200305bc7d6bfba7b3d4b
David Aronchick <aronchick@gmail.com>
2024-11-30 14:02:09 -0800

    refactor: Enhance SFTP client interface with directory creation and file mode support

--- pkg/sshutils/sftp.go
+++ pkg/sshutils/sftp.go
@@ -2,7 +2,10 @@ package sshutils
 
 import (
 	"io"
+	"os"
+	"path/filepath"
 
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -10,8 +13,40 @@ import (
 type SFTPClienter interface {
 	Create(path string) (io.WriteCloser, error)
 	Open(path string) (io.ReadCloser, error)
+	MkdirAll(path string) error
+	Chmod(path string, mode os.FileMode) error
 	Close() error
 }
 
-// SFTPDial is a function type for creating SFTP clients
-type SFTPDialFunc func(conn *ssh.Client) (SFTPClienter, error)
+// SFTPClientCreator is a function type for creating SFTP clients
+type SFTPClientCreator func(conn *ssh.Client) (SFTPClienter, error)
+
+// defaultSFTPClient wraps the sftp.Client to implement our interface
+type defaultSFTPClient struct {
+	*sftp.Client
+}
+
+func (c *defaultSFTPClient) MkdirAll(path string) error {
+	return c.Client.MkdirAll(path)
+}
+
+// Create creates a new file on the remote system
+func (c *defaultSFTPClient) Create(path string) (io.WriteCloser, error) {
+	// Ensure parent directory exists
+	if err := c.MkdirAll(filepath.Dir(path)); err != nil {
+		return nil, err
+	}
+	return c.Client.Create(path)
+}
+
+// NewSFTPClient creates a new SFTP client from an SSH client
+func NewSFTPClient(client *ssh.Client) (SFTPClienter, error) {
+	sftpClient, err := sftp.NewClient(client)
+	if err != nil {
+		return nil, err
+	}
+	return &defaultSFTPClient{Client: sftpClient}, nil
+}
+
+// Default SFTP client creator
+var DefaultSFTPClientCreator SFTPClientCreator = NewSFTPClient
